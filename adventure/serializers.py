09bda0570da2ef84722619ff6c8c5cb815f3dc82
Noble Obioma <nobioma1@gmail.com>
2019-10-24 13:21:02 +0100

    add x and y to serializer fields

--- adventure/serializers.py
+++ adventure/serializers.py
@@ -5,4 +5,4 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ['id', 'title', 'description', 'n_to', 's_to', 'e_to', 'w_to']
+        fields = ['id', 'title', 'description', 'n_to', 's_to', 'e_to', 'w_to', 'x', 'y']
